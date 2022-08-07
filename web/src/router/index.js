29ae36d10260aa5dc1141fa2b3ec56fa4c982a9b
w zs <483704512@qq.com>
2022-08-07 23:40:11 +0800

    4.3 配置Mysql与注册登录模块（下）

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -6,57 +6,90 @@ import RecordIndexView from '../views/record/RecordIndexView'
 import UserBotIndexView from '../views/user/bot/UserBotIndexView'
 import UserAccountLoginView from '../views/user/account/UserAccountLoginView'
 import UserAccountRegisterView from '../views/user/account/UserAccountRegisterView'
+import store from '../store/index'
 
 const routes = [
   {
     path: "/",
     name: "home",
-    redirect: "/pk/"
+    redirect: "/pk/",
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/pk/",
     name: "pk_index",
     component: PkIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/record/",
     name: "record_index",
     component: RecordIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/404/",
     name: "404_index",
     component: NotFound,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/ranklist/",
     name: "ranklist_index",
     component: RanklistIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/user/account/login/",
     name: "user_account_login",
     component: UserAccountLoginView,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/user/account/register/",
     name: "user_account_register",
     component: UserAccountRegisterView,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/user/bot/",
     name: "user_bot_index",
     component: UserBotIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/:catchAll(.*)",
     redirect: "/404/"
   },
 ]
-
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  //如果要进入的页面需要授权，并且还没有登录，就返回到登录页面
+  if (to.meta.requestAuth && !store.state.user.is_login) {
+    next({ name: "user_account_login" });
+  } else {
+    next();
+  }
 })
 
 export default router
